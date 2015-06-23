2eaf07f4c92914d3cefd29fe6705311677c390f7
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-06-22 22:05:48 -0430

    giras

--- www/js/app.js
+++ www/js/app.js
@@ -48,21 +48,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.gira', {
+      url: '/gira',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-gira': {
+          templateUrl: 'templates/tab-gira.html',
+          controller: 'GiraCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.gira-detail', {
+      url: '/gira/:chatId',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-gira': {
+          templateUrl: 'templates/gira-detail.html',
+          controller: 'GiraDetailCtrl'
         }
       }
     })
@@ -78,6 +78,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/gira');
 
 });
